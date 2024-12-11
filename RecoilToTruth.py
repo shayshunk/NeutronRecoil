1fac2298a7f3deb4526302314b89d713cfd2ff6f
Shashank <shayjayakumar@gmail.com>
2024-12-10 18:38:10 -1000

    Fixed error message by ignoring.

--- RecoilToTruth.py
+++ RecoilToTruth.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from decimal import Decimal
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard  # type: ignore
 import time
 import glob
 import os
